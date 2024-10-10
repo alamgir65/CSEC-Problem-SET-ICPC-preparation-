eb56fa882ceee2fb1916e1051291450531cb08fb
alamgir65 <alamgirhossaindrubo@gmail.com>
2024-10-10 23:05:22 +0600

    done

---
+++ Tasks_and_Deadlines.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define cyes cout<<"Yes"<<endl;
+#define cno cout<<"No"<<endl;
+#define print(flag) (flag)? cout<<"YES\n" : cout<<"NO\n";
+#define all(x) x.begin(), x.end()
+#define nl cout<<endl;
+#define endl '\n'
+#define love int main()
+#define vtr vector
+#define pii pair<int,int>
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define vii vector<int> 
+#define vll vector<ll>
+#define dqii deque<int> 
+#define dqll deque<ll> 
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define Alamgir ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+using namespace std;
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+void solve(){
+    int n; cin>>n;
+    vector<pair<int,int>> v;
+    for(int i=0;i<n;i++){
+        int a,b; cin>>a>>b;
+        v.push_back({a,b});
+    }
+    sort(all(v));
+    ll ans=v[0].second - v[0].first;
+    ll last=v[0].first;
+    for(int i=1;i<n;i++){
+        // cout<<last<<" "<<ans<<endl;
+        last += v[i].first;
+        ans += (v[i].second-last);
+    }
+    out(ans)
+}
+love{
+    Alamgir
+    int t=1;
+    // cin>>t;
+    for(int i=1;i<=t;i++){
+        // cout<<"Case "<<i<<": ";
+        solve();
+    }
+    return 0;    
+}
